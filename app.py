fb6f5bb897e1b782a56d6eb7cbe46cd0e08dbcdc
Ciro <Ciro16@gmail.com>
2021-01-21 11:23:36 -0800

    added postgres to heroku

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'postgres:///blogly')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
